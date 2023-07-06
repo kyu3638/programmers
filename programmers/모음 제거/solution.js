221fa85f7e7b98acc31605ef3a4b58a345afe6b2
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2023-07-07 02:09:54 +0900

    uploaded at 2023. 7. 7. 오전 2:09:54

---
+++ programmers/모음 제거/solution.js
@@ -0,0 +1,15 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120849
+
+function solution(my_string) {
+    var answer = '';
+    
+    for(let i = 0 ; i < my_string.length; i++){
+        if(my_string[i] == "a"||my_string[i]=="e"||my_string[i]=="i"||my_string[i]=="u"||my_string[i]=="o"){
+            continue;
+        } else{
+            answer = answer+my_string[i];
+        }
+    }
+    
+    return answer;
+}
